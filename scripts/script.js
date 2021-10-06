47e951f6b1ea7f92c2cea459d55b1d6103d947a6
bignab <nabilrafatisb@gmail.com>
2021-10-06 15:04:12 -0400

    basic functionality, grid added with hover effect

---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+createDivGrid(16,16);
+
+function createDivGrid(gridSizeX, gridSizeY) {
+    let divGrid = [];
+    for (let i = 0; i < gridSizeX; i++) {
+        let divGridRow = [];
+        for (let j = 0; j < gridSizeY; j++) {
+            let element = document.createElement("div");
+            let divID = "" + (i+1) + "," + (j+1);
+            element.classList.add("gridCell")
+            element.id = divID;
+            // element.textContent = divID;
+            element.style.gridColumnStart = (j+1);
+            element.style.gridColumnEnd = (j+2);
+            element.style.gridRowStart = (i+1);
+            element.style.gridRowEnd = (i+2);
+            element.addEventListener('mouseover', () => {
+                element.classList.add("hoveredCell")
+            });
+
+            divGridRow.push(element);
+        }
+        divGrid.push(divGridRow);
+    }
+    placeDivGridIntoDOM(gridSizeX, gridSizeY, divGrid);
+}
+
+function placeDivGridIntoDOM(gridSizeX, gridSizeY, filledDivGrid) {
+    const container = document.querySelector('#container');
+    for (let i = 0; i < gridSizeX; i++) {
+        for (let j = 0; j < gridSizeY; j++) {
+            container.appendChild(filledDivGrid[i][j]);
+        }
+    }
+}
