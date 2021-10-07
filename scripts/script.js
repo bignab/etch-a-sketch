10e3fe71787ad7aa130eb760f9a4fc3508b6971b
bignab <nabilrafatisb@gmail.com>
2021-10-07 16:59:30 -0400

    added button functionality, updated design, added slider for resetting size

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,7 @@ function createDivGrid(gridSizeX, gridSizeY) {
             element.style.gridRowStart = (i+1);
             element.style.gridRowEnd = (i+2);
             element.addEventListener('mouseover', () => {
-                element.classList.add("hoveredCell")
+                changeColourOfDiv(element);
             });
 
             divGridRow.push(element);
@@ -37,7 +37,56 @@ function clearDivGrid() {
     let containerChildren = container.childNodes;
     containerChildren.forEach(function(currentValue) {
         if (currentValue.nodeType === Node.ELEMENT_NODE) {
-            currentValue.classList.remove("hoveredCell");
+            currentValue.style.backgroundColor = "white";
         }
     });
+}
+
+function changeColourOfDiv(divElement) {
+    const monochromeButton = document.querySelector('#monochromeButton');
+        if (monochromeButton.classList.contains("monochromeToggle")) {
+            divElement.style.backgroundColor = "#7036B1";
+        }
+        else {
+            let randomColour = "#" + Math.floor(Math.random()*16777215).toString(16);
+            divElement.style.backgroundColor = randomColour;
+        }
+}
+
+const monochromeButton = document.querySelector('#monochromeButton');
+const colourButton = document.querySelector('#colourButton');
+const clearButton = document.querySelector('#clearButton');
+const myRangeSlider = document.querySelector('#myRange');
+const setGridButton = document.querySelector('#setGridButton');
+
+monochromeButton.addEventListener('click', () => {
+    monochromeButton.classList.add("monochromeToggle");
+    colourButton.classList.remove("colourToggle");
+});
+
+colourButton.addEventListener('click', () => {
+    monochromeButton.classList.remove("monochromeToggle");
+    colourButton.classList.add("colourToggle");
+});
+
+clearButton.addEventListener('click', () => {
+    clearDivGrid();
+});
+
+setGridButton.addEventListener('click', () => {
+    resetDivGrid();
+});
+
+myRangeSlider.addEventListener('mousemove', () => {
+    let myRangeSliderValue = myRangeSlider.value;
+    document.getElementById("gridSizeText").textContent = myRangeSliderValue + " x " + myRangeSliderValue;
+});
+
+function resetDivGrid() {
+    const container = document.querySelector('#container');
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    let myRangeSliderValue = myRangeSlider.value;
+    createDivGrid(myRangeSliderValue, myRangeSliderValue);
 }
